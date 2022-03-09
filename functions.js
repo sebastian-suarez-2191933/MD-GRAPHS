5579716a3e1bdceb1a0204352f17a7d229931105
ThesplumCoder <michin505@hotmail.com>
2022-03-09 15:35:01 -0500

    Implementé la librería Vis.js, ejemplo base.

--- functions.js
+++ functions.js
@@ -1,21 +1,55 @@
+// create an array with nodes
+var nodes = new vis.DataSet([{
+    id: 1,
+    label: "Node 1"
+  },
+  {
+    id: 2,
+    label: "Node 2"
+  },
+  {
+    id: 3,
+    label: "Node 3"
+  },
+  {
+    id: 4,
+    label: "Node 4"
+  },
+  {
+    id: 5,
+    label: "Node 5"
+  },
+]);
 
-window.onload = async () =>
-{
-    var b1 = document.getElementById('b1')
-    b1.addEventListener('click',pintarNodo,true)
-    
-    function pintarNodo()
-    {
-    canvas.addEventListener('click', (e) => {
-    let x = e.clientX - canvas.offsetLeft;
-    let y = e.clientY - canvas.offsetTop;
-    
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(x, y, 20, 0, 2 * Math.PI);
-    ctx.stroke();
-    }
-    );
-    }
-}
+// create an array with edges
+var edges = new vis.DataSet([{
+    from: 1,
+    to: 3
+  },
+  {
+    from: 1,
+    to: 2
+  },
+  {
+    from: 2,
+    to: 4
+  },
+  {
+    from: 2,
+    to: 5
+  },
+  {
+    from: 3,
+    to: 3
+  },
+]);
+
+// create a network
+var container = document.getElementById("ElGrafo");
+var data = {
+  nodes: nodes,
+  edges: edges,
+};
+var options = {};
+var network = new vis.Network(container, data, options);
+console.log('Prueba');
